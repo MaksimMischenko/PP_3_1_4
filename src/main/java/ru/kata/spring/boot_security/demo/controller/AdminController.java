3bee52946f1c653c6af169173ab8553f6adb86af
MaksimMischenko <t89969534097@gmail.com>
2022-08-21 10:37:24 +0400

    +

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,9 +3,12 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
@@ -19,7 +22,8 @@ public class AdminController {
     }
 
     @GetMapping
-    public String getUserList(Model model) {
+    public String getUserList(Model model, Principal principal) {
+        model.addAttribute("user", userService.loadUserByUsername(principal.getName()));
         model.addAttribute("users", userService.getListUsers());
         return "admin";
     }
